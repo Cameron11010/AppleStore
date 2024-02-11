65dd55380d84d7ae19898810cb74634b21151bc2
Cameron11010 <115788189+Cameron11010@users.noreply.github.com>
2024-02-11 15:26:21 +0000

    Watch.h

---
+++ Watch.h
@@ -0,0 +1,23 @@
+#ifndef Watch_h
+#define Watch_h
+
+#include "Product.h"
+
+class Watch : public Product {
+public:
+    Watch();
+    Watch(std::string name, std::string productId, std::string color, int size, bool isUltra, float price, int quantity);
+    ~Watch();
+    void setColor(std::string color) override;
+    void setSize(int size);
+    void setIsUltra(bool isUltra);
+    int getSize() const;
+    bool getIsUltra() const;
+    void display() const override;
+private:
+    std::string color;
+    int size;
+    bool isUltra;
+};
+
+#endif /* Watch_h */
