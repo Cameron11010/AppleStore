3020e293a3d5aa85f57a462b0d0627f98450f8d6
Cameron11010 <115788189+Cameron11010@users.noreply.github.com>
2024-02-12 01:11:20 +0000

    Airpods.h

--- Airpods.h
+++ Airpods.h
@@ -6,15 +6,15 @@
 class Airpods : public Product {
 public:
     Airpods();
-    Airpods(std::string name, std::string productId, std::string color, std::string chargingType, float price, int quantity);
+    Airpods(std::string name, std::string productId, std::string colour, std::string chargingType, float price, int quantity);
     ~Airpods();
-    void setColor(std::string color) override;
+    void setColour(std::string colour) override;
     void setChargingType(std::string chargingType);
-    std::string getColor() const;
+    std::string getColour() const;
     std::string getChargingType() const;
     void display() const override;
 private:
-    std::string color;
+    std::string colour;
     std::string chargingType;
 };
 
