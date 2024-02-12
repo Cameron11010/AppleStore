7e1aedecd4674307651b1c22c6485bee3bbeae6d
Cameron11010 <115788189+Cameron11010@users.noreply.github.com>
2024-02-12 01:11:09 +0000

    Airpods.cpp

--- Airpods.cpp
+++ Airpods.cpp
@@ -1,22 +1,22 @@
 #include "Airpods.h"
 
-Airpods::Airpods() : Product(), color(""), chargingType("") {}
+Airpods::Airpods() : Product(), colour(""), chargingType("") {}
 
-Airpods::Airpods(std::string name, std::string productId, std::string color, std::string chargingType, float price, int quantity)
-        : Product(name, productId, price, quantity), color(color), chargingType(chargingType) {}
+Airpods::Airpods(std::string name, std::string productId, std::string colour, std::string chargingType, float price, int quantity)
+        : Product(name, productId, price, quantity), colour(colour), chargingType(chargingType) {}
 
 Airpods::~Airpods() {}
 
-void Airpods::setColor(std::string color) {
-    this->color = color;
+void Airpods::setColour(std::string colour) {
+    this->colour = colour;
 }
 
 void Airpods::setChargingType(std::string chargingType) {
     this->chargingType = chargingType;
 }
 
-std::string Airpods::getColor() const {
-    return color;
+std::string Airpods::getColour() const {
+    return colour;
 }
 
 std::string Airpods::getChargingType() const {
@@ -26,7 +26,7 @@ std::string Airpods::getChargingType() const {
 void Airpods::display() const {
     std::cout << "Name: " << getName() << std::endl;
     std::cout << "Product ID: " << getProductId() << std::endl;
-    std::cout << "Color: " << color << std::endl;
+    std::cout << "Colour: " << colour << std::endl;
     std::cout << "Charging Type: " << chargingType << std::endl;
     std::cout << "Price: $" << getPrice() << std::endl;
     std::cout << "Quantity: " << getQuantity() << std::endl;
