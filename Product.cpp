108ee1eea5a7ba860e457776dcf2a8eadb488864
Cameron11010 <115788189+Cameron11010@users.noreply.github.com>
2024-02-12 01:12:15 +0000

    Product.cpp

--- Product.cpp
+++ Product.cpp
@@ -1,25 +1,24 @@
-#ifndef Product_h
-#define Product_h
+#include "Product.h"
 
-#include <iostream>
-#include <string>
+Product::Product() : name(""), productId(""), price(0.0), quantity(0) {}
 
-class Product {
-public:
-    Product();
-    Product(std::string name, std::string productId, float price, int quantity);
-    virtual ~Product();
-    std::string getName() const;
-    std::string getProductId() const;
-    float getPrice() const;
-    int getQuantity() const;
-    virtual void setColor(std::string color) = 0;
-    virtual void display() const = 0;
-protected:
-    std::string name;
-    std::string productId;
-    float price;
-    int quantity;
-};
+Product::Product(std::string name, std::string productId, float price, int quantity)
+        : name(name), productId(productId), price(price), quantity(quantity) {}
 
-#endif /* Product_h */
+Product::~Product() {}
+
+std::string Product::getName() const {
+    return name;
+}
+
+std::string Product::getProductId() const {
+    return productId;
+}
+
+float Product::getPrice() const {
+    return price;
+}
+
+int Product::getQuantity() const {
+    return quantity;
+}
