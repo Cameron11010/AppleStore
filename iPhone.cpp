91f6fb5198216955f617f68857c42e64f95c58b0
Cameron11010 <115788189+Cameron11010@users.noreply.github.com>
2024-02-12 01:13:02 +0000

    iPhone.cpp

--- iPhone.cpp
+++ iPhone.cpp
@@ -1,22 +1,22 @@
 #include "iPhone.h"
 
-iPhone::iPhone() : Product(), color(""), chargingType("") {}
+iPhone::iPhone() : Product(), colour(""), chargingType("") {}
 
-iPhone::iPhone(std::string name, std::string productId, std::string color, std::string chargingType, float price, int quantity)
-        : Product(name, productId, price, quantity), color(color), chargingType(chargingType) {}
+iPhone::iPhone(std::string name, std::string productId, std::string colour, std::string chargingType, float price, int quantity)
+        : Product(name, productId, price, quantity), colour(colour), chargingType(chargingType) {}
 
 iPhone::~iPhone() {}
 
-void iPhone::setColor(std::string color) {
-    this->color = color;
+void iPhone::setColour(std::string colour) {
+    this->colour = colour;
 }
 
 void iPhone::setChargingType(std::string chargingType) {
     this->chargingType = chargingType;
 }
 
-std::string iPhone::getColor() const {
-    return color;
+std::string iPhone::getColour() const {
+    return colour;
 }
 
 std::string iPhone::getChargingType() const {
@@ -26,7 +26,7 @@ std::string iPhone::getChargingType() const {
 void iPhone::display() const {
     std::cout << "Name: " << getName() << std::endl;
     std::cout << "Product ID: " << getProductId() << std::endl;
-    std::cout << "Color: " << color << std::endl;
+    std::cout << "Colour: " << colour << std::endl;
     std::cout << "Charging Type: " << chargingType << std::endl;
     std::cout << "Price: $" << getPrice() << std::endl;
     std::cout << "Quantity: " << getQuantity() << std::endl;
