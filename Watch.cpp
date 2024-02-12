fc2d962b6a2ccaee2332d1be0e15c09ba91a21d2
Cameron11010 <115788189+Cameron11010@users.noreply.github.com>
2024-02-12 01:12:39 +0000

    Watch.cpp

--- Watch.cpp
+++ Watch.cpp
@@ -1,24 +1,38 @@
-#ifndef Watch_h
-#define Watch_h
+#include "Watch.h"
 
-#include "Product.h"
+Watch::Watch() : Product(), colour(""), size(0), isUltra(false) {}
 
-class Watch : public Product {
-public:
-    Watch();
-    Watch(std::string name, std::string productId, std::string color, int size, bool isUltra, float price, int quantity);
-    ~Watch();
-    void setColor(std::string color) override;
-    void setSize(int size);
-    void setIsUltra(bool isUltra);
-    std::string getColor() const;
-    int getSize() const;
-    bool getIsUltra() const;
-    void display() const override;
-private:
-    std::string color;
-    int size;
-    bool isUltra;
-};
+Watch::Watch(std::string name, std::string productId, std::string colour, int size, bool isUltra, float price, int quantity)
+        : Product(name, productId, price, quantity), colour(colour), size(size), isUltra(isUltra) {}
 
-#endif /* Watch_h */
+Watch::~Watch() {}
+
+void Watch::setColour(std::string colour) {
+    this->colour = colour;
+}
+
+void Watch::setSize(int s) {
+    size = s;
+}
+
+void Watch::setIsUltra(bool i) {
+    isUltra = i;
+}
+
+int Watch::getSize() const {
+    return size;
+}
+
+bool Watch::getIsUltra() const {
+    return isUltra;
+}
+
+void Watch::display() const {
+    std::cout << "Name: " << getName() << std::endl;
+    std::cout << "Product ID: " << getProductId() << std::endl;
+    std::cout << "Colour: " << colour << std::endl;
+    std::cout << "Size: " << size << std::endl;
+    std::cout << "Is Ultra: " << (isUltra ? "Yes" : "No") << std::endl;
+    std::cout << "Price: $" << getPrice() << std::endl;
+    std::cout << "Quantity: " << getQuantity() << std::endl;
+}
